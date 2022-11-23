39f86c80156283ee77fd8c99d25cb53ea6607320
1ekk <akatsukiserver1@gmail.com>
2022-11-24 00:44:37 +0300

    minor changes

--- button_proxy/button_proxy.js
+++ button_proxy/button_proxy.js
@@ -1,31 +1,22 @@
 const app = require('express')()
 const http = require('http')
 
-const config = require('./config/config')
+const config = require('../config/config')
 
-const DEVICE_PORT = config.DEVICE_PORT
 const DEVICES = config.DEVICES
 
 app.get('/api/device/:dev_id/button/:btn_id', (req, res) => {
-    let data = ''
-
     DEVICES.forEach(device => {
         if (device.id == req.params.dev_id) {
             console.log(`Pressed btn #${req.params.btn_id} on device #${req.params.dev_id} with ip: ${device.ip}`)
-            res.json('TEST')
-
-            let _URL = `http://${device.ip}:${DEVICE_PORT}/instrumentctrl/vnc/php/sethardkeyaction.php?ID=${req.params.btn_id}`
+            res.json('')
+            let _URL = `http://${device.ip}/instrumentctrl/vnc/php/sethardkeyaction.php?ID=${req.params.btn_id}`
             console.log(_URL)
-            http.get(_URL, (resp) => {
-                resp.on('data', (chunk) => {
-                    data += chunk
+            http.get(_URL)
+                .on('error', err => {
+                    console.log('ERROR: ', err)
                 })
-                resp.on('end', () => {
-                    res.sendStatus(200)
-                })
-            }).on('error', (err) => {
-                console.log('ERROR: ', err)
-            })
+                
         }
     })
 })
--- config/config.js
+++ config/config.js
@@ -1,11 +1,7 @@
 const config = {
-    DEVICE_PORT: 6080,
-    DEVICES:    [{ip: '192.168.10.144', id: 1}, 
-                {ip: '192.168.10.110', id: 2}, 
-                {ip: '192.168.10.120', id: 3},],
-    BUTTONS: [
-
-    ]
+    DEVICES:    [{ip: '192.168.88.144', id: 1}, 
+                 {ip: '192.168.10.110', id: 2}, 
+                 {ip: '192.168.10.120', id: 3}]
 }
 
 module.exports = config
