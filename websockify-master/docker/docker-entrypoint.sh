8870c2250ffaaf9bbda7b1fa2fef0b7ecc70d090
1ekk <akatsukiserver1@gmail.com>
2022-11-24 23:08:16 +0300

    new enrtypoint

--- websockify-master/docker/docker-entrypoint.sh
+++ websockify-master/docker/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 
 set -e
 
-/opt/websockify/websockify-$VERSION/run "$@"
+/opt/websockify/websockify-$VERSION/run --token-plugin=ReadOnlyTokenFile --token-source=/data/token_file.cfg  localhost:6080
